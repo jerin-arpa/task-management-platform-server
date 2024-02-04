fd2d7c83fe35e869845594ec0bf5ac0bbefdfa3a
jerin-arpa <ismatjerinarpa@gmail.com>
2024-02-05 03:40:45 +0600

    send add task data inn mongodb

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
-app.get('/', (req, res) => {
-    res.send('Task Management Platform Is Running')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4d9dszy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const boardCollection = client.db("taskManagementDB").collection("board");
+
+    app.post("/board", async (req, res) => {
+      const newBoard = req.body;
+      console.log(newBoard);
+      const result = await boardCollection.insertOne(newBoard);
+      res.send(result);
+    });
+
+    // app.get("/board", async (req, res) => {
+    //   const result = await boardDataCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Task Management Platform Is Running");
+});
 
 app.listen(port, () => {
-    console.log(`Task Management Platform Is Running on port ${port}`)
-})
+  console.log(`Task Management Platform Is Running on port ${port}`);
+});
